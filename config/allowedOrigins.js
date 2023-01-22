c446aea0333e5f390afd3b9cd8973189f6997393
mansgori <manoj.gori1993@gmail.com>
2023-01-22 11:47:16 +0530

    ready to deploy

---
+++ config/allowedOrigins.js
@@ -0,0 +1,11 @@
+
+// creating array for allowedorigins otherthan this 
+// not allowed to login
+
+const allowedOrigins = [
+    'http://localhost:3000',
+    'https://www.dandrepairshop.com',
+    'https://dandrepairshop.com'
+]
+
+module.exports = allowedOrigins
