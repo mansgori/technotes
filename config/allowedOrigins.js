eed560fd63656fc82a25591ebc3e60b253e904d2
mansgori <manoj.gori1993@gmail.com>
2023-01-22 12:12:04 +0530

    cors policy change

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,9 +3,7 @@
 // not allowed to login
 
 const allowedOrigins = [
-    'http://localhost:3000',
-    'https://www.dandrepairshop.com',
-    'https://dandrepairshop.com'
+    'https://technotes.onrender.com'
 ]
 
 module.exports = allowedOrigins
